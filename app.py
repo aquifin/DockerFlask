2f07af77fd96df5f110a96f29dde9bd5d65291e7
Aquif Inamdar <48316886+aquifin@users.noreply.github.com>
2024-09-12 16:49:15 +0530

    Updated

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
-app = Flask(__name__,template_folder='templates')
+app_flask = Flask(__name__,template_folder='templates')
 	
 
-def create_database():
+def connect_db():
     conn = sqlite3.connect('test.db')
     c = conn.cursor()
+    
+    return conn,c
+    
+def create_database():
+    conn,c = connect_db()
     c.execute('''CREATE TABLE IF NOT EXISTS students ( id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
         age INTEGER NOT NULL)''')
@@ -14,36 +19,34 @@ def create_database():
     conn.close()
 	
 	
-@app.route('/')
-def index():
-    conn = sqlite3.connect('test.db')
-    c = conn.cursor()
+@app_flask.route('/')
+def landingpage():
+    conn,c = connect_db()
     c.execute('SELECT * FROM students')
     students = c.fetchall()
     conn.close()
     return render_template('index.html', rows=students)
 
-@app.route('/add', methods=['POST'])
+@app_flask.route('/add', methods=['POST'])
 def add_task():
     name = request.form['name']
     age = request.form['age']
-    conn = sqlite3.connect('test.db')
-    c = conn.cursor()
+    
+    conn,c = connect_db()
     c.execute('INSERT INTO students (name,age) VALUES (?,?)', (name,age))
     conn.commit()
     conn.close()
-    return redirect(url_for('index'))
+    return redirect(url_for('landingpage'))
 
-@app.route('/delete/<int:id>')
+@app_flask.route('/delete/<int:id>')
 def delete_task(id):
-    conn = sqlite3.connect('test.db')
-    c = conn.cursor()
+    conn,c = connect_db()
     c.execute('DELETE FROM students WHERE id = ?', (id,))
     conn.commit()
     conn.close()
-    return redirect(url_for('index'))
+    return redirect(url_for('landingpage'))
 	
 	
 if __name__ == "__main__":
     create_database()
-    app.run(debug=True)
+    app_flask.run(debug=True)
